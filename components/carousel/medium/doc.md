91e48f08c9dc014d5fce82f17b94bdaea70482e1
jeneser <jeneserwang@gmail.com>
2017-09-03 20:35:59 +0800

    Add component medium-carousel

---
+++ components/carousel/medium/doc.md
@@ -0,0 +1,5 @@
+## Carousel 走马灯
+
+在有限空间内，循环播放同一类型的图片、文字等内容
+
+<div id="carousel-medium"></div>
---
+++ components/carousel/medium/index.js
@@ -0,0 +1,10 @@
+import html from './src/carousel.component.html';
+import css from './src/carousel.component.css';
+import carousel from './src/carousel.component.js';
+import doc from './doc.md';
+
+export default function() {
+  document.querySelector('#demo-container').innerHTML = doc;
+  document.querySelector('#carousel-medium').innerHTML = html;
+  carousel();
+}
